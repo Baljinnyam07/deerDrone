218bb4d7b6f43080df3fcd20e6868f733cdbfcf0
Baljinnyam07 <altex1230@gmail.com>
2026-04-08 16:51:33 +0800

    fix

--- apps/admin/next.config.mjs
+++ apps/admin/next.config.mjs
@@ -1,6 +1,15 @@
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@deer-drone/data", "@deer-drone/ui", "@deer-drone/utils", "@deer-drone/types"],
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+  },
 };
 
 export default nextConfig;
