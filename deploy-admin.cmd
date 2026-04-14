2b4f46e06eb4000e8096990db5e764b481baf904
Baljinnyam07 <altex1230@gmail.com>
2026-04-14 11:02:17 +0800

    add deploy cmd

---
+++ deploy-admin.cmd
@@ -0,0 +1,5 @@
+@echo off
+set "VERCEL_ORG_ID=team_2y2B14f74Q0NfFqOwQKCzWb4"
+set "VERCEL_PROJECT_ID=prj_93oPjp57GuXmQ1umBMi9EawPhi18"
+echo Deploying Admin App...
+npx vercel --prod
---
+++ deploy-web.cmd
@@ -0,0 +1,5 @@
+@echo off
+set "VERCEL_ORG_ID=team_2y2B14f74Q0NfFqOwQKCzWb4"
+set "VERCEL_PROJECT_ID=prj_Gh9RAh4gXRyTvVDhW2U1BOoO8zor"
+echo Deploying Web App...
+npx vercel --prod
