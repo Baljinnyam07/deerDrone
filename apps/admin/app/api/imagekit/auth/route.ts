484df6e963489491b18a30cfa096857287528d56
Baljinnyam07 <altex1230@gmail.com>
2026-04-30 11:25:45 +0800

    fix admin build

--- apps/admin/app/api/imagekit/auth/route.ts
+++ apps/admin/app/api/imagekit/auth/route.ts
@@ -1,14 +1,16 @@
 import ImageKit from "imagekit";
 import { NextResponse } from "next/server";
 
-const imagekit = new ImageKit({
-  publicKey: process.env.IMAGEKIT_PUBLIC_KEY!,
-  privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
-  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT!,
-});
-
 export async function GET() {
   try {
+    if (!process.env.IMAGEKIT_PUBLIC_KEY) {
+      throw new Error("Missing ImageKit configuration");
+    }
+    const imagekit = new ImageKit({
+      publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+      privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
+      urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT!,
+    });
     const authParams = imagekit.getAuthenticationParameters();
     return NextResponse.json(authParams);
   } catch (error) {
